d7944bbdb3456009fc899baebb5de8ec12888cba
Nir Geller <gnir.work@gmail.com>
2019-12-23 16:22:12 +0200

    Added all of the colums and created Flag component

--- frontend/src/api/people.tsx
+++ frontend/src/api/people.tsx
@@ -1,16 +1,42 @@
 import _ from "lodash";
 
 export interface PersonInterface {
-    name: string;
-    lastName: string;
+    fullName: string;
     id: string;
     age: number;
+    period: string;
+    week: number;
+    arrivalTime: string;
+    departureTime: string;
+    invitor: string;
+    bed: string;
+    entryPass: boolean;
+    verifiedArrival: boolean;
+    miluim: boolean;
+    makishur: boolean;
+    arrived: boolean;
+    megama: string;
+    reason: string;
+    remarks: string;
 }
 
 export const getPeople = (): Array<PersonInterface> =>
-    _.range(46).map(id => ({
-        name: "ניר",
-        lastName: "גלר",
+    _.range(1000).map(id => ({
         id: id.toString(),
-        age: 21
+        fullName: "ניר גלר",
+        age: 21,
+        period: "Ahud",
+        week: 7,
+        arrivalTime: (new Date()).toDateString(),
+        departureTime: (new Date()).toDateString(),
+        invitor: "gnir",
+        bed: "לא צריך",
+        entryPass: !!_.random(0,1),
+        verifiedArrival: !!_.random(0,1),
+        miluim: !!_.random(0,1),
+        makishur: !!_.random(0,1),
+        arrived: !!_.random(0,1),
+        megama: "פסטן",
+        reason: "פסטן פסטן פסטן",
+        remarks: "שדגדשג שדג שדג שדג שדג שדג שדג שדג שדג שדג שדג"
     }));
--- frontend/src/components/PeopleTable.tsx
+++ frontend/src/components/PeopleTable.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import classNames from "classnames";
-import { Table } from "antd";
+import { Table, Tag, Switch } from "antd";
 import { PersonInterface } from "../api/people";
+import Flag from "./Flag";
 
-import './PeopleTable.scss'
+import "./PeopleTable.scss";
 
 interface PeopleTableProps {
     people: Array<PersonInterface>;
@@ -12,19 +13,92 @@ interface PeopleTableProps {
 
 const PeopleTableColumns = [
     {
-        title: "שם פרטי",
-        dataIndex: "name",
-        key: "name"
-    },
-    {
-        title: "שם משפחה",
-        dataIndex: "lastName",
-        key: "lastName"
+        title: "שם מלא",
+        dataIndex: "fullName",
+        key: "fullName"
     },
     {
         title: "גיל",
         dataIndex: "age",
         key: "age"
+    },
+    {
+        title: "תקופה",
+        dataIndex: "period",
+        key: "period"
+    },
+    {
+        title: "שבוע",
+        dataIndex: "week",
+        key: "week"
+    },
+    {
+        title: "ממתי",
+        dataIndex: "arrivalTime",
+        key: "arrivalTime"
+    },
+    {
+        title: "עד מתי",
+        dataIndex: "departureTime",
+        key: "departureTime"
+    },
+    {
+        title: "מזמין",
+        dataIndex: "invitor",
+        key: "invitor"
+    },
+    {
+        title: "מיטה",
+        dataIndex: "bed",
+        key: "beb"
+    },
+    {
+        title: "אישור כניסה",
+        dataIndex: "entryPass",
+        key: "entryPass",
+        render: (checked: boolean) => <Flag checked={checked} />,
+        width: "4em"
+    },
+    {
+        title: "ווידאו הגעה",
+        dataIndex: "verifiedArrival",
+        key: "verifiedArrival",
+        render: (checked: boolean) => <Flag checked={checked} />,
+        width: "4em"
+    },
+    {
+        title: "בקשת מילואים",
+        dataIndex: "miluim",
+        key: "miluim",
+        render: (checked: boolean) => <Flag checked={checked} />,
+    },
+    {
+        title: "בקשת מהקישור",
+        dataIndex: "makishur",
+        key: "makishur",
+        render: (checked: boolean) => <Flag checked={checked} />,
+    },
+    {
+        title: "הגיעה",
+        dataIndex: "arrived",
+        key: "arrived",
+        render: (checked: boolean) => <Flag checked={checked} />,
+        width: "4em"
+    },
+    {
+        title: "מגמה \\ מסלול",
+        dataIndex: "megama",
+        key: "megama"
+    },
+    {
+        title: "סיבה",
+        dataIndex: "reason",
+        key: "reason"
+    },
+    {
+        title: "הערות נוספות",
+        dataIndex: "remarks",
+        key: "remarks"
     }
 ];
 
@@ -32,11 +106,7 @@ const PeopleTable: React.FC<PeopleTableProps> = ({ people, className }) => {
     const data = people.map(person => ({ ...person, key: person.id }));
     return (
         <div className={classNames(className, "people-table-container")}>
-            <Table
-                dataSource={data}
-                columns={PeopleTableColumns}
-                pagination={false}
-            />
+            <Table dataSource={data} columns={PeopleTableColumns} size="middle" pagination={{pageSize: 30}} />
         </div>
     );
 };
