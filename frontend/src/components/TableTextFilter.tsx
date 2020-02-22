d554e7f5805216252b46ea25b55782767ae35879
Nir Geller <gnir.work@gmail.com>
2020-02-22 19:30:04 +0200

    Added auto focus on text filters.

--- frontend/src/components/TableTextFilter.tsx
+++ frontend/src/components/TableTextFilter.tsx
@@ -1,52 +1,54 @@
-import React from 'react';
-import { Input, Button } from 'antd';
+import React from "react";
+import { Input, Button } from "antd";
 
 import "./TableFilter.scss";
 
 interface TableTextFilterProps {
-    setSelectedKeys: Function,
-    selectedKeys: string[],
-    confirm: () => void,
-    clearFilters: () => void
+  setSelectedKeys: Function;
+  selectedKeys: string[];
+  confirm: () => void;
+  clearFilters: () => void;
 }
 
 const TableTextFilter: React.FC<TableTextFilterProps> = ({
-    setSelectedKeys,
-    selectedKeys,
-    confirm,
-    clearFilters
+  setSelectedKeys,
+  selectedKeys,
+  confirm,
+  clearFilters
 }) => {
-    const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const newKeys = e.target.value ? [e.target.value] : [];
-        setSelectedKeys(newKeys);
-    };
+  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newKeys = e.target.value ? [e.target.value] : [];
+    setSelectedKeys(newKeys);
+  };
 
-    return (<div className="table-filter">
-        <Input
-            placeholder={`הכנס את החיפוש`}
-            value={selectedKeys[0]}
-            onChange={onSearch}
-            className="search-input"
-            onPressEnter={confirm}
-        />
-        <Button
-            type="primary"
-            onClick={confirm}
-            icon="search"
-            size="small"
-            className="search-button"
-        >
-            חפש
-        </Button>
-        <Button
-            onClick={clearFilters}
-            size="small"
-            className="clear-button"
-        >
-            נקה חיפוש
-        </Button>
+  return (
+    <div className="table-filter">
+      <Input
+        ref={(instance: Input) => {
+          if (instance) {
+            instance.focus();
+          }
+        }}
+        placeholder={`הכנס את החיפוש`}
+        value={selectedKeys[0]}
+        onChange={onSearch}
+        className="search-input"
+        onPressEnter={confirm}
+      />
+      <Button
+        type="primary"
+        onClick={confirm}
+        icon="search"
+        size="small"
+        className="search-button"
+      >
+        חפש
+      </Button>
+      <Button onClick={clearFilters} size="small" className="clear-button">
+        נקה חיפוש
+      </Button>
     </div>
-    );
+  );
 };
 
 export default TableTextFilter;
