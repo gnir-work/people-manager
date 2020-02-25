dfb6cc89c7d173ba31ff9633e8c37c39e8f3325e
Nir Geller <gnir.work@gmail.com>
2020-02-25 19:12:20 +0200

    Added documentation for the deletion function

--- frontend/src/components/PeopleTable/PeopleTableDeleteButton.tsx
+++ frontend/src/components/PeopleTable/PeopleTableDeleteButton.tsx
@@ -16,6 +16,11 @@ const PeopleTableDeleteButton: React.FC<PeopleTableDeleteButtonProps> = ({
   person
 }) => {
   const { deletePerson } = useContext(PeopleContext);
+
+  /**
+   * Delete the person passed as a props to this component and show a toaster with
+   * the result of the deletion (Success or Failure).
+   */
   const handleClick = useCallback(() => {
     if (deletePerson(person)) {
       message.success(`${person.fullName} נמחק בהצלחה!`);
