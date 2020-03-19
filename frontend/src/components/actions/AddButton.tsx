9571bdecac9eb3ad305282e632a392120335cf41
gnir <gnir.work@gmail.com>
2020-03-19 19:17:10 +0200

    Fixed bug that form would reopen when submitted with Enter

--- frontend/src/components/actions/AddButton.tsx
+++ frontend/src/components/actions/AddButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 
@@ -8,16 +8,34 @@ interface AddButtonProps {
   onClick: () => void;
 }
 
-const AddButton: React.FC<AddButtonProps> = ({ onClick }) => (
-  <Button
-    type="primary"
-    shape="circle"
-    onClick={onClick}
-    size="large"
-    className="add-button"
-  >
-    <PlusOutlined />
-  </Button>
-);
+const AddButton: React.FC<AddButtonProps> = ({ onClick }) => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const handleButton = () => {
+    /**
+     * We want to un focus the button once it was clicked in order
+     * to prevent accidental clicks when we press Enter for example.
+     */
+    if (buttonRef && buttonRef.current) {
+      buttonRef.current.blur();
+    }
+    onClick();
+  };
+
+  return (
+    /**
+     * This is a plain button and not Antd's Button because
+     * we need a way to blur the button once clicked.
+     */
+    <button
+      ref={buttonRef}
+      onClick={handleButton}
+      type="button"
+      className="ant-btn add-button ant-btn-primary ant-btn-circle ant-btn-lg"
+    >
+      <PlusOutlined />
+    </button>
+  );
+};
 
 export default AddButton;
