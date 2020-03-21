43596cc966286a2fcf972e274a32fabf7101550c
gnir <gnir.work@gmail.com>
2020-03-21 16:00:11 +0200

    Update the id of the person model to be his personal_id

--- backend/people_manager/people/models.py
+++ backend/people_manager/people/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Person(models.Model):
+  personal_id = models.CharField(max_length=20, primary_key=True)
   full_name = models.CharField(max_length=200)
--- backend/people_manager/people/serializers.py
+++ backend/people_manager/people/serializers.py
@@ -5,4 +5,4 @@ from .models import Person
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
-        fields = ['full_name']
+        fields = ['full_name', 'personal_id']
