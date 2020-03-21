3b9140f2f1c95b888663911df588cb8be3cd028a
gnir <gnir.work@gmail.com>
2020-03-21 16:35:40 +0200

    Added authetication security on data.

--- backend/people_manager/people/views.py
+++ backend/people_manager/people/views.py
@@ -1,13 +1,17 @@
 from .models import Person
 from .serializers import PersonSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 
 class PersonList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
 
 
 class PersonDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
