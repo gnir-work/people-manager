05770e099269675b0517df332df7e171b82419a6
gnir <gnir.work@gmail.com>
2020-03-21 16:39:06 +0200

    Request authentication only in production.

--- backend/people_manager/people/views.py
+++ backend/people_manager/people/views.py
@@ -1,17 +1,17 @@
 from .models import Person
 from .serializers import PersonSerializer
 from rest_framework import generics, permissions
-
+from people_manager import settings
 
 class PersonList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated] if not settings.DEBUG else []
 
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
 
 
 class PersonDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-    
+    permission_classes = [permissions.IsAuthenticated] if not settings.DEBUG else []
+
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
