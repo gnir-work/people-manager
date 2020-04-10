59e07fd43d2955228273588783137b85945a6732
Nir Geller <gnir.work@gmail.com>
2020-04-10 15:43:54 +0300

    Extracted form to a different component.

--- frontend/src/PeoplePage/AddPersonModal.tsx
+++ frontend/src/PeoplePage/AddPersonModal.tsx
@@ -1,38 +1,10 @@
-import React, { useContext, useMemo, useCallback } from "react";
-import AddButton from "../components/actions/AddButton";
-import { Modal, Form, Input, Radio, Checkbox, AutoComplete } from "antd";
-import TextArea from "antd/lib/input/TextArea";
-import TagList from "../components/tags/TagList";
+import React, { useContext, useCallback } from "react";
 import { PeopleContext } from "../contexts/PeopleContext";
-import {
-  GET_PERSONAL_ID_RULES,
-  GET_BASIC_TEXT_RULES,
-  GET_PHONE_NUMBER_RULES,
-  GET_REQUIRED_RULE
-} from "../components/validators/validators";
 import { PeopleSettingsContext } from "../contexts/PeopleSettingsContext";
 import FormModal, {
   ChildrenFunction as FormModalChildren
 } from "../components/modals/FormModal";
-
-const layout = {
-  labelCol: { span: 5 },
-  wrapperCol: { span: 19 }
-};
-
-const initialValues = {
-  fullName: "",
-  personId: "",
-  phone: "",
-  status: "",
-  team: "",
-  preferences: [],
-  tracks: [],
-  subjects: [],
-  availability: "",
-  wasSegel: false,
-  remarks: ""
-};
+import AddPersonForm from "./AddPersonForm";
 
 const AddPersonModal: React.FC = () => {
   const { addPerson, doesPersonExist, getFieldDataSet } = useContext(
@@ -46,91 +18,13 @@ const AddPersonModal: React.FC = () => {
   };
 
   const renderForm = useCallback(
-    ({ onEnter, form }: FormModalChildren) => (
-      <Form
-        {...layout}
-        size="small"
-        form={form}
-        name="add_person_form"
-        initialValues={initialValues}
-        onKeyDown={onEnter}
-      >
-        <Form.Item
-          name="fullName"
-          label="שם מלא"
-          rules={GET_BASIC_TEXT_RULES()}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="personalId"
-          label="מ.א"
-          rules={GET_PERSONAL_ID_RULES(doesPersonExist)}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item name="phone" label="פלאפון" rules={GET_PHONE_NUMBER_RULES()}>
-          <Input />
-        </Form.Item>
-        <Form.Item name="team" label="צוות">
-          <AutoComplete
-            onKeyDown={event => {
-              event.stopPropagation();
-            }}
-            options={possibleTeams.map(team => ({ value: team }))}
-          />
-        </Form.Item>
-        <Form.Item
-          rules={[GET_REQUIRED_RULE("בבקשה הכנס מצב שירות")]}
-          name="status"
-          label="מצב שירות"
-        >
-          <Radio.Group>
-            {settings.possibleStatuses.map(status => (
-              <Radio key={status} value={status}>
-                {status}
-              </Radio>
-            ))}
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item
-          rules={[GET_REQUIRED_RULE("בבקשה הכנס זמינות")]}
-          name="availability"
-          label="זמינות"
-        >
-          <Radio.Group>
-            {settings.possibleAvailabilities.map(availability => (
-              <Radio key={availability} value={availability}>
-                {availability}
-              </Radio>
-            ))}
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item name="wasSegel" label="סגל עבר" valuePropName="checked">
-          <Checkbox />
-        </Form.Item>
-        <Form.Item name="preferences" label="העדפות" valuePropName="tags">
-          <TagList
-            possibleTags={settings.possiblePreferences}
-            additionText="הוספת העדפה"
-          />
-        </Form.Item>
-        <Form.Item name="tracks" label="מסלולים" valuePropName="tags">
-          <TagList
-            possibleTags={settings.possibleTracks}
-            additionText="הוספת מסלול"
-          />
-        </Form.Item>
-        <Form.Item name="subjects" label="מערכים" valuePropName="tags">
-          <TagList
-            possibleTags={settings.possibleSubjects}
-            additionText="הוספת מערך"
-          />
-        </Form.Item>
-        <Form.Item name="remarks" label="הערות נוספות">
-          <TextArea />
-        </Form.Item>
-      </Form>
+    (formProps: FormModalChildren) => (
+      <AddPersonForm
+        settings={settings}
+        possibleTeams={possibleTeams}
+        doesPersonExist={doesPersonExist}
+        {...formProps}
+      />
     ),
     [settings, possibleTeams, doesPersonExist]
   );
