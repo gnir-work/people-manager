64d3ea0c45ecd94ec185ef63a757f0ce6c287772
Nir Geller <gnir.work@gmail.com>
2020-04-15 00:04:00 +0300

    Extract mapping of fields to functions.

--- backend/people_manager/src/appointments.py
+++ backend/people_manager/src/appointments.py
@@ -5,22 +5,14 @@ from .exceptions import AppointmentExists
 from bson import ObjectId
 from pymongo.errors import DuplicateKeyError
 
-def get_id_to_person_mapping():
-    people = get_people_collection().find({})
-    return {
-        str(person["_id"]): format_document_id(person) for person in people
-    }
-
 
 def get_all_appointments():
     appointments_collection = get_appointments_collection()
     appointments = list(appointments_collection.find({}))
-    id_to_person = get_id_to_person_mapping()
+    id_to_person = _get_id_to_person_mapping()
     for appointment in appointments:
         appointment["person"] = id_to_person[appointment["person"]]
-        appointment["dates"] = [appointment["from"], appointment["to"]]
-        del appointment["from"]
-        del appointment["to"]
+        _map_fields_after_db(appointment)
         appointment = format_document_id(appointment)
     return appointments
 
@@ -29,9 +21,7 @@ def update_appointment(appointment_id: str, new_values: dict):
     appointments = get_appointments_collection()
     try:
         if "dates" in new_values:
-            new_values["from"] = new_values["dates"][0]
-            new_values["to"] = new_values["dates"][1]
-            del new_values["dates"]
+            _map_fields_before_db(new_values)
 
         result = appointments.update_one(
             {"_id": ObjectId(appointment_id)}, {"$set": new_values}
@@ -50,9 +40,7 @@ def delete_appointment(appointment_id: str):
 def create_appointment(appointment: dict):
     appointments = get_appointments_collection()
     try:
-        appointment["from"] = appointment["dates"][0]
-        appointment["to"] = appointment["dates"][1]
-        del appointment["dates"]
+        _map_fields_before_db(appointment)
         result = appointments.insert_one(appointment)
         if result.acknowledged and result.inserted_id:
             return str(result.inserted_id)
@@ -60,3 +48,20 @@ def create_appointment(appointment: dict):
             return None
     except DuplicateKeyError:
         raise AppointmentExists
+
+
+def _get_id_to_person_mapping():
+    people = get_people_collection().find({})
+    return {str(person["_id"]): format_document_id(person) for person in people}
+
+
+def _map_fields_before_db(appointment: dict):
+    appointment["from"] = appointment["dates"][0]
+    appointment["to"] = appointment["dates"][1]
+    del appointment["dates"]
+
+
+def _map_fields_after_db(appointment: dict):
+    appointment["dates"] = [appointment["from"], appointment["to"]]
+    del appointment["from"]
+    del appointment["to"]
